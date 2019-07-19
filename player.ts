13a4b794a3e8660a5618ca2c852a305a3c08503c
John Garabagiu <ion.g.garabagiu@gmail.com>
2019-07-19 23:30:10 +0300

    Add proper getter setters

--- player.ts
+++ player.ts
@@ -2,67 +2,149 @@ import { Dice } from "./dice";
 import { Ante } from "./ante";
 
 export class Player {
-	playerData: any;
-	play: Dice[];
-	power: number;
-	availablePower: number;
-	diceList: any;
-	antes: Ante[];
-	classType: string;
-	discard: number;
-	clock: number;
-	pressure: number;
-	pressureTokens: number;
+
+	private _playerData: any;
+	private _play: Dice[];
+	private _power: number;
+	private _availablePower: number;
+	private _diceList: any;
+	private _antes: Ante[];
+	private _classType: string;
+	private _discard: number;
+	private _clock: number;
+	private _pressure: number;
+	private _pressureTokens: number;
 
 	constructor(power: number, data: any, diceList: any, antes: Ante[], classType: string) {
-		this.pressure = 0;
-		this.pressureTokens = 0;
-		this.power = power;
-		this.playerData = data;
-		this.play = [];
-		this.availablePower = power;
-		this.diceList = diceList;
-		this.antes = antes.map(ante => new Ante(ante.name, ante.power, ante.text))
-		this.classType = classType;
-		this.discard = 0
-		this.clock = 2
+		this._pressure = 0;
+		this._pressureTokens = 0;
+		this._power = power;
+		this._playerData = data;
+		this._play = [];
+		this._availablePower = power;
+		this._diceList = diceList;
+		this._antes = antes.map(ante => new Ante(ante.name, ante.power, ante.text))
+		this._classType = classType;
+		this._discard = 0
+		this._clock = 2
 
 	}
 
+	public set playerData(v : any) {
+		this._playerData = v;
+	}
+
+	public get playerData() : any {
+		return this._playerData
+	}
+	
+	public set play(v : Dice[]) {
+		this._play = v;
+	}
+	
+	public get play() : Dice[] {
+		return this._play
+	}
+	
+	public set power(v : number) {
+		this._power = v;
+	}
+	
+	public get power() : number {
+		return this._power
+	}
+	
+	public set availablePower(v : number) {
+		this._availablePower = v;
+	}
+	
+	public get availablePower() : number {
+		return this._availablePower
+	}
+		
+	public set antes(v : Ante[]) {
+		this._antes = v;
+	}
+	
+	public get antes() : Ante[] {
+		return this._antes
+	}
+	
+	public set classType(v : string) {
+		this._classType = v;
+	}
+	
+	public get classType() : string {
+		return this._classType
+	}
+	
+	public set discard(v : number) {
+		this._discard = v;
+	}
+	
+	public get discard() : number {
+		return this._discard
+	}
+	
+	public set clock(v : number) {
+		this._clock = v;
+	}
+	
+	public get clock() : number {
+		return this._clock
+	}
+	
+	public set pressure(v : number) {
+		this._pressure = v;
+	}
+	
+	public get pressure() : number {
+		return this._pressure
+	}
+	
+	public set pressureTokens(v : number) {
+		this._pressureTokens = v;
+	}
+	
+	public get pressureTokens() : number {
+		return this._pressureTokens
+	}
+	
+	
 	push(n: number, size: number = 6) {
-		this.availablePower -= n;
-		this.pressureTokens = 0;
+		this._availablePower -= n;
+		this._pressureTokens = 0;
 		for (let i = 0; i < n; i++) {
 			let roll = this.dice(size)
-			this.play.push(this.diceList[`d${size}`][roll - 1])
+			this._play.push(this._diceList[`d${size}`][roll - 1])
 		}
-		this.play.sort((a, b) => a.value - b.value)
-		this.pressure = this.play.length + this.pressureTokens
+		this._play.sort((a, b) => a.value - b.value)
+		this._pressure = this._play.length + this._pressureTokens
 	}
 
 	press() {
-		this.pressureTokens += 1
-		this.pressure = this.play.length + this.pressureTokens
-		return this.pressure
+		this._pressureTokens += 1
+		this._pressure = this._play.length + this._pressureTokens
+		return this._pressure
 	}
 
 	ante(name: string) {
-		this.pressureTokens = 0
+		this._pressureTokens = 0
 		let anteExists: boolean = false
 		let foundAnte: Ante;
-		for (let i = 0; i < this.antes.length; i++) {
-			const ante = this.antes[i];
+		for (let i = 0; i < this._antes.length; i++) {
+			const ante = this._antes[i];
 			if (ante.name.toUpperCase().startsWith(name.toUpperCase()) && ante.available) {
 				anteExists = true
 				foundAnte = ante
 				ante.available = false
-				this.availablePower += ante.power
+				this._availablePower += ante.power
 				this.push(ante.power)
 				break
 			}
 		}
 
-		this.pressure = this.play.length + this.pressureTokens
+		this._pressure = this._play.length + this._pressureTokens
 		if (anteExists) {
 			return foundAnte
 		} else {
@@ -74,47 +156,47 @@ export class Player {
 	counter(dice: number[], xType?: number) {
 		for (let i = 0; i < dice.length; i++) {
 			const die = dice[i];
-			for (let j = 0; j < this.play.length; j++) {
-				const playerDie = this.play[j];
+			for (let j = 0; j < this._play.length; j++) {
+				const playerDie = this._play[j];
 				if (die === playerDie.value) {
 					if (xType) {
 
 					} else {
-						this.play.splice(j, 1)
-						this.discard++
+						this._play.splice(j, 1)
+						this._discard++
 						break
 					}
 				}
 			}
 		}
-		this.pressure = this.play.length + this.pressureTokens
+		this._pressure = this._play.length + this._pressureTokens
 	}
 
 	cut(recover: boolean) {
 		if (recover) {
-			this.discard -= this.clock;
-			this.availablePower++;
+			this._discard -= this._clock;
+			this._availablePower++;
 		}
-		this.clock++;
+		this._clock++;
 	}
 
 	reroll(dice: number[], type?: string) {
-		let length = this.play.length
+		let length = this._play.length
 		let newDice: Dice[] = []
-		let newPlay: Dice[] = [...this.play]
+		let newPlay: Dice[] = [...this._play]
 
 		if (dice.length > 0) {
 			for (let i = 0; i < dice.length; i++) {
 				const die = dice[i];
-				for (let j = 0; j < this.play.length; j++) {
-					const playerDie = this.play[j];
+				for (let j = 0; j < this._play.length; j++) {
+					const playerDie = this._play[j];
 					if (playerDie.value === die) {
 						if (type) {
 
 						} else {
 							let roll = this.dice(playerDie.size)
 							newPlay.splice(j, 1)
-							newPlay.push(this.diceList[playerDie.type][roll - 1])
+							newPlay.push(this._diceList[playerDie.type][roll - 1])
 							break
 						}
 					}
@@ -124,17 +206,17 @@ export class Player {
 			//Reroll all dice
 			let j = 0
 			while (newPlay.length > 0) {
-				const playerDie = this.play[j];
+				const playerDie = this._play[j];
 				let roll = this.dice(playerDie.size)
 				newPlay.splice(0, 1)
-				newDice.push(this.diceList[playerDie.type][roll - 1])
+				newDice.push(this._diceList[playerDie.type][roll - 1])
 				j++
 			}
 		}
 
 		newPlay.push(...newDice)
-		this.play = [...newPlay]
-		this.play.sort((a, b) => a.value - b.value)
+		this._play = [...newPlay]
+		this._play.sort((a, b) => a.value - b.value)
 	}
 
 	private dice(n) {
